930620800bb7d613ce1f05d82403b16a22c0e4c5
dak-7309 <daksh18137@iiitd.ac.in>
2020-10-18 01:10:20 +0530

    18th Oct Update

--- utils.py
+++ utils.py
@@ -3,26 +3,29 @@ import numpy as np
 import math
 from scipy.spatial import distance as dist
 
-def draw_prediction(frame, classes, classId, conf, left, top, right, bottom,color,temp,L):
+def draw_prediction(frame, classes, classId, conf, left, top, right, bottom,color,temp,L,i):
+    
+    label = str(i)  
 
     if classes:
         assert(classId < len(classes))
-    # if classes[classId]=='person':
     cv2.rectangle(frame, (left, top), (right, bottom), color,2)
+    cv2.putText(frame, label, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.6,color,1)
     
+
     if temp==False:
         text="Number of Social Distancing Violations= "+str(L)
-        cv2.putText(frame,text,(15,frame.shape[0]-15),cv2.FONT_HERSHEY_COMPLEX, 0.6, (255,255,255), 1)
+        cv2.putText(frame,text,(15,frame.shape[0]-15),cv2.FONT_HERSHEY_COMPLEX, 0.6, (0,255,0), 1)
         
 
 
 def process_frame(frame, outs, classes, confThreshold, nmsThreshold):
     frameHeight = frame.shape[0]
     frameWidth = frame.shape[1]
-
     classIds = []
     confidences = []
     boxes = []
+
     for out in outs:
         for detection in out:
             scores = detection[5:]
@@ -40,10 +43,10 @@ def process_frame(frame, outs, classes, confThreshold, nmsThreshold):
                 boxes.append([left, top, width, height,center_x,center_y])
 
     indices = cv2.dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThreshold)
-
     violate=set()
 
-
+    heights_=[]
+    widths_=[]
     results=[]
     for i in indices:
         i = i[0]
@@ -53,25 +56,36 @@ def process_frame(frame, outs, classes, confThreshold, nmsThreshold):
             y = box[1]
             width = box[2]
             height = box[3]
+            heights_.append(height)
+            widths_.append(width)
             centerx=int(x+width/2)
             centery=int(y+height/2)
             r=(i,(x,y,x+width,y+height),(centerx,centery))
             results.append(r)
-        
+
+    # avgg=sum(heights_)/len(heights_)
 
     temp=False
     if len(results)>=2:
 
         c=np.array([r[2] for r in results ])
         d=dist.cdist(c, c, metric="euclidean")
+        distances_list=[]
 
         for i in range(d.shape[0]):
             for j in range(i+1,d.shape[1]):
-                if d[i,j]<90:
+
+                h_=(heights_[i]+heights_[j])/2
+                val=6*(h_/5.5)
+
+                if d[i,j]<val:
+                    cv2.line(frame,results[i][2],results[j][2],(255,255,255),1)
                     violate.add(i)
                     violate.add(j)
-        
+                    DIST=max(0,round( ((6/val)*(d[i,j])  ),2))
+                    distances_list.append([i,j,DIST])
 
+        
         for (i,(ind,bbox,center)) in enumerate(results):
             (startx,starty,endx,endy)=bbox
             (cx,cy)=center
@@ -79,12 +93,21 @@ def process_frame(frame, outs, classes, confThreshold, nmsThreshold):
 
             if i in violate:
                 color=(0,0,255)
-            draw_prediction(frame, classes, classIds[ind], confidences[ind], startx, starty, endx,endy,color,temp,len(violate))
+            draw_prediction(frame, classes, classIds[ind], confidences[ind], startx, starty, endx,endy,color,temp,len(violate),i)
             temp=True
+
+        for xx in range(len(distances_list)):
+            label=("Distance from "+str(distances_list[xx][0])+" to "+ str(distances_list[xx][1])+" = "+str(distances_list[xx][2])+"ft")
+            cv2.putText(frame,label,(15,20+(25*xx)),cv2.FONT_HERSHEY_COMPLEX, 0.6,(0,255,0), 1)
+
+
     return len(violate)
+
 
     
 
+# h_ -> 5.5
+# 1 -> 5.5/h_
 
 
 
